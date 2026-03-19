83a9a361bb4d17b2145e0db5e72ec6efd189fc7d
Vics004 <victormpalaciosm04@gmail.com>
2026-03-18 20:13:12 -0600

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Coco_Beach.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
