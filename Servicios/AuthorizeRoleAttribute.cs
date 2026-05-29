c113e3e90f4cbe77542512e9e7041566eac2f295
Fernando <felelfer13@example.com>
2026-05-29 10:53:56 -0600

    Implementación de baneo en tiempo real

--- Servicios/AuthorizeRoleAttribute.cs
+++ Servicios/AuthorizeRoleAttribute.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Mvc;
+using Coco_Beach.Models;
 
 namespace Coco_Beach.Servicios
 {
@@ -21,6 +22,22 @@ namespace Coco_Beach.Servicios
             {
                 context.Result = new RedirectToActionResult("Autenticar", "Login", null);
                 return;
+            }
+            // 👇 Resolver el DbContext igual que en el controller
+            var db = context.HttpContext.RequestServices.GetService<Coco_BeachDbContext>();
+            if (db != null)
+            {
+                var personaId = context.HttpContext.Session.GetInt32("personaId");
+                if (personaId != null)
+                {
+                    var persona = db.persona.Find(personaId);
+                    if (persona != null && persona.estado == false)
+                    {
+                        context.HttpContext.Session.Clear();
+                        context.Result = new RedirectToActionResult("Autenticar", "Login", null);
+                        return;
+                    }
+                }
             }
 
             if (_allowedRoles.Length > 0 && !_allowedRoles.Contains(rolUsuario))
